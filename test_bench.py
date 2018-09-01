95e0aad5cfaa383eb16c51bd99a4817043d555c0
AgentDS <zszx0259@me.com>
2018-09-01 12:26:12 -0700

    change verbosity to 2 for unit test

--- test_bench.py
+++ test_bench.py
@@ -10,4 +10,4 @@ import unittest
 
 if __name__ == "__main__":
     suite = unittest.TestLoader().discover('./test/', pattern='*_test.py')
-    unittest.TextTestRunner(verbosity=0).run(suite)
+    unittest.TextTestRunner(verbosity=2).run(suite)
