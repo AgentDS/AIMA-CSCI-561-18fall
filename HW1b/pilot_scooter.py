f64a6a687345161c89afdeafd8b9df2feb9de833
AgentDS <zszx0259@me.com>
2018-09-10 13:32:33 -0700

    add installed package name on Vocareum in file packages

---
+++ HW1b/pilot_scooter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 9/10/18 11:39 AM
+# @Author  : Siqi Liang
+# @Contact : zszxlsq@gmail.com
+# @File    : pilot_scooter.py
+# @Software: PyCharm
+
+import numpy as np
