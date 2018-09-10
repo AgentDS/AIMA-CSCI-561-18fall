08410ea0b8baa64a7b0b7cfd2310f6ec36a40d44
AgentDS <zszx0259@me.com>
2018-09-09 21:57:53 -0700

    change file directory

---
+++ HW1b/hw1cs561f2018.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 9/9/18 9:57 PM
+# @Author  : Siqi Liang
+# @Contact : zszxlsq@gmail.com
+# @File    : hw1cs561f2018.py
+# @Software: PyCharm
