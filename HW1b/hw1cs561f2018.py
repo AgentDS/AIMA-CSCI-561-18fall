2edec73e824c07c465576cdb0b46a132cc520d23
AgentDS <zszx0259@me.com>
2018-09-11 11:42:21 -0700

    testing location map

--- HW1b/hw1cs561f2018.py
+++ HW1b/hw1cs561f2018.py
@@ -7,7 +7,9 @@
 # @Software: PyCharm
 
 import pilot_scooter as ps
+import numpy as np
 
-if __name__=='__main__':
+if __name__ == '__main__':
     in_path = 'input1.txt'
     problem = ps.problem_generator(in_path)
+    office_map = ps.OLocationMap(problem.N, np.array([[0, 0], [2, 3], [1, 4]], dtype=np.int))
--- HW1b/pilot_scooter.py
+++ HW1b/pilot_scooter.py
@@ -15,10 +15,17 @@ class ScooterProblem(object):
         self.N = N  # the width and height of the n x n city area
         self.P = P  # number of police officers
         self.S = S  # number of scooters
-        self.location = self._make_location(locations)  # list of S elements, each element is a 12 x 2 np.ndarray
+        self.idx_location = self._make_location2(locations)
+        location1 = self._make_location1(locations)  # list of S elements, each element is a 12 x 2 np.ndarray
+        self.smap = SLocationMap(self.N, location1)
+        self.omap = None
 
-    def _make_location(self, locations):
+
+    def _make_location1(self, locations):
         return [locations[s:s + 12, :] for s in range(self.S)]
+
+    def _make_location2(self, locations):
+        return [[locations[t + 12 * s] for s in range(self.S)] for t in range(12)]
 
 
 class SLocationMap(object):
@@ -26,6 +33,7 @@ class SLocationMap(object):
 
     def __init__(self, N, locations):
         self.N = N
+        self.S = len(locations)
         self.bin_str_map = self._idx2bin_str(locations)
         self.int_map = self._bin_str2int(self.bin_str_map)
 
@@ -37,7 +45,7 @@ class SLocationMap(object):
                 # for each location index of scooter, [col, row]
                 col = scooter[t, 0]
                 row = scooter[t, 1]
-                bin_str_map[t][row][col] = '1'
+                bin_str_map[t][row] = bin_str_map[t][row][:col] + '1' + bin_str_map[t][row][col + 1:]
         return bin_str_map
 
     def _bin_str2int(self, bin_str_map):
@@ -53,35 +61,36 @@ class SLocationMap(object):
 class OLocationMap(object):
     """location map for officers"""
 
-    # TODO: design location map for police officers
     def __init__(self, N, location):
         self.N = N
-        self.bin_str_map = None
-        self.int_map = None
-    #     self.bin_str_map = self._idx2bin_str(locations)
-    #     self.int_map = self._bin_str2int(self.bin_str_map)
-    #
-    # def _idx2bin_str(self, locations):
-    #     N = self.N
-    #     bin_str_map = ['0' * N for n in range(N)]
-    #     for scooter in locations:
-    #             # for each location index of scooter, [col, row]
-    #             col = scooter[t, 0]
-    #             row = scooter[t, 1]
-    #             bin_str_map[t][row][col] = '1'
-    #     return bin_str_map
-    #
-    # def _bin_str2int(self, bin_str_map):
-    #     N = self.N
-    #     int_map = [[0 for n in range(N)] for i in range(12)]
-    #     for t in range(12):
-    #         for row in range(self.N):
-    #             # convert bin string, like (0b)'1010', to integer with base 10, that is, 10
-    #             int_map[t][row] = int(bin_str_map[t][row], base=2)
-    #     return int_map
+        self.P = len(location)
+        self.bin_str_map = self._idx2bin_str(location)
+        self.int_map = self._bin_str2int(self.bin_str_map)
+
+    def _idx2bin_str(self, location):
+        N = self.N
+        bin_str_map = ['0' * N for n in range(N)]
+        for officer in location:
+            # for each location index of officer, [col, row]
+            col = officer[0]
+            row = officer[1]
+            bin_str_map[row] = bin_str_map[row][:col] + '1' + bin_str_map[row][col + 1:]
+        return bin_str_map
+
+    def _bin_str2int(self, bin_str_map):
+        int_map = [0 for n in range(self.N)]
+        for row in range(self.N):
+            int_map[row] = int(bin_str_map[row], base=2)
+        return int_map
 
 
-def point_calculator(scooter_map: SLocationMap, officer_map: OLocationMap):
+def point_calculator(scooter_map, officer_map):
+    """
+
+    :param scooter_map: SLocationMap
+    :param officer_map: OLocationMap
+    :return:
+    """
     points = 0
     N = scooter_map.N
     # TODO: is 'for' faster or 'map' faster????
@@ -116,8 +125,3 @@ def scooter_plot(problem):
     P = problem.P
     S = problem.S
     # TODO: plot the map in matplotlib?
-
-#
-# def loc2idx(location):
-#     S = len(location)
-#     idxes = [np.zero(12, 2) for s in range(S)]
