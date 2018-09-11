accb489211fe7ff51d239e525cebf451623a83ac
AgentDS <zszx0259@me.com>
2018-09-11 01:10:58 -0700

    finish location map design

--- HW1b/pilot_scooter.py
+++ HW1b/pilot_scooter.py
@@ -21,6 +21,76 @@ class ScooterProblem(object):
         return [locations[s:s + 12, :] for s in range(self.S)]
 
 
+class SLocationMap(object):
+    """location map for scooters"""
+
+    def __init__(self, N, locations):
+        self.N = N
+        self.bin_str_map = self._idx2bin_str(locations)
+        self.int_map = self._bin_str2int(self.bin_str_map)
+
+    def _idx2bin_str(self, locations):
+        N = self.N
+        bin_str_map = [['0' * N for n in range(N)] for i in range(12)]
+        for scooter in locations:
+            for t in range(12):
+                # for each location index of scooter, [col, row]
+                col = scooter[t, 0]
+                row = scooter[t, 1]
+                bin_str_map[t][row][col] = '1'
+        return bin_str_map
+
+    def _bin_str2int(self, bin_str_map):
+        N = self.N
+        int_map = [[0 for n in range(N)] for i in range(12)]
+        for t in range(12):
+            for row in range(self.N):
+                # convert bin string, like (0b)'1010', to integer with base 10, that is, 10
+                int_map[t][row] = int(bin_str_map[t][row], base=2)
+        return int_map
+
+
+class OLocationMap(object):
+    """location map for officers"""
+
+    # TODO: design location map for police officers
+    def __init__(self, N, location):
+        self.N = N
+        self.bin_str_map = None
+        self.int_map = None
+    #     self.bin_str_map = self._idx2bin_str(locations)
+    #     self.int_map = self._bin_str2int(self.bin_str_map)
+    #
+    # def _idx2bin_str(self, locations):
+    #     N = self.N
+    #     bin_str_map = ['0' * N for n in range(N)]
+    #     for scooter in locations:
+    #             # for each location index of scooter, [col, row]
+    #             col = scooter[t, 0]
+    #             row = scooter[t, 1]
+    #             bin_str_map[t][row][col] = '1'
+    #     return bin_str_map
+    #
+    # def _bin_str2int(self, bin_str_map):
+    #     N = self.N
+    #     int_map = [[0 for n in range(N)] for i in range(12)]
+    #     for t in range(12):
+    #         for row in range(self.N):
+    #             # convert bin string, like (0b)'1010', to integer with base 10, that is, 10
+    #             int_map[t][row] = int(bin_str_map[t][row], base=2)
+    #     return int_map
+
+
+def point_calculator(scooter_map: SLocationMap, officer_map: OLocationMap):
+    points = 0
+    N = scooter_map.N
+    # TODO: is 'for' faster or 'map' faster????
+    for row in range(N):
+        for t in range(12):
+            points += bin(scooter_map.int_map[t][row] & officer_map.int_map[row]).count('1')
+    return points
+
+
 def problem_generator(in_path):
     line_ct = 0
     for line in open(in_path, 'r'):
@@ -39,3 +109,15 @@ def problem_generator(in_path):
 def problem_output(m, out_path):
     with open(out_path, 'w') as out_f:
         print('%d' % m, end='', file=out_f)
+
+
+def scooter_plot(problem):
+    N = problem.N
+    P = problem.P
+    S = problem.S
+    # TODO: plot the map in matplotlib?
+
+#
+# def loc2idx(location):
+#     S = len(location)
+#     idxes = [np.zero(12, 2) for s in range(S)]
